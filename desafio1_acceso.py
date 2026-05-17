329a15ecee208941518fa694260cd1ee1d3a51fb
pablohf7 <pablohf7@gmail.com>
2026-05-17 14:16:06 -0500

    Comentarios al programa

--- desafio1_acceso.py
+++ desafio1_acceso.py
@@ -1,12 +1,12 @@
 # Programa creado para controlar acceso
-
 #Creamos el bucle while para repetir la ejecución del programa
 opcion = ''
 while opcion != 'no':
 
-    # Programa creado para controlar acceso
+    # Imprimimos el nombre del programa
     print('PROGRAMA PARA CONTROL DE ACCESO')
     print('###############################')
+    # Solitamos los datos para hacer el control de acceso
     nombre = input('Ingrese su nombre:\n') # Solicitamos el nombre
     edad = int(input('Ingrese su edad:\n')) # Solicitamos la edad
     tiene_entrada = input('¿Tiene entrada?(s/n):\n') # Preguntamos si tiene entrada
@@ -19,6 +19,7 @@ while opcion != 'no':
         print(f'{nombre}, eres menor de edad y por lo tanto no puedes ingresar')
     print('-----------------------------------------------------------------------------------------------------')
     print('')
+    # Preguntamos para validar a otra persona
     opcion = input('¿Desea validar otra persona?(si/no):\n')
    
 
