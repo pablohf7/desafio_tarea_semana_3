136f3a9ebd94eafabf409eae12bf99adca959baf
pablohf7 <pablohf7@gmail.com>
2026-05-17 13:56:22 -0500

    programa completado

--- desafio1_acceso.py
+++ desafio1_acceso.py
@@ -5,18 +5,22 @@ opcion = ''
 while opcion != 'no':
 
     # Programa creado para controlar acceso
+    print('PROGRAMA PARA CONTROL DE ACCESO')
+    print('###############################')
     nombre = input('Ingrese su nombre:\n') # Solicitamos el nombre
     edad = int(input('Ingrese su edad:\n')) # Solicitamos la edad
     tiene_entrada = input('¿Tiene entrada?(s/n):\n') # Preguntamos si tiene entrada
     lista_vip = input('¿Esta en la lista VIP?(s/n):\n') # Consultamos si esta en la lista VIP
 
     #Creamos los condicionales para control del acceso 
-    if edad >= 18:
-        print(f'{nombre}, eres mayor de edad') 
-    elif tiene_entrada == 's' or lista_vip == 's':
-        print(f'Puedes ingresar')
+    if edad >= 18 and (tiene_entrada == 's' or lista_vip == 's') :
+        print(f'{nombre}, puedes ingresar') 
     else:
-        print('No puedes ingresar')
+        print(f'{nombre}, eres menor de edad y por lo tanto no puedes ingresar')
+    print('-----------------------------------------------------------------------------------------------------')
+    print('')
+    opcion = input('¿Desea validar otra persona?(si/no):\n')
+   
 
 
   
