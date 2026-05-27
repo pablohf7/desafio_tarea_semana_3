83dd2777ca533e06c572b01b35eaaea4de5abb2e
pablohf7 <pablohf7@gmail.com>
2026-05-26 21:54:50 -0500

    Comentarios en desafio4_password.py

--- desafio4_password.py
+++ desafio4_password.py
@@ -1,19 +1,19 @@
 # Programa para control contraseña
-contraseña_establecida = 'hola123'
-intentos = 3
+contraseña_establecida = 'hola123' # establecemos la variable con la contraseña
+intentos = 3 # Iniciamos el contador con 3 intentos
 
-while intentos > 0:
-    contraseña_ingresada = input('Ingrese su contraseña:\n')
-    if contraseña_ingresada == 'salir':
+while intentos > 0: # Inciacimos el ciclo while con la condición
+    contraseña_ingresada = input('Ingrese su contraseña:\n') # Solicitamos el ingreso de la contraseña
+    if contraseña_ingresada == 'salir': # Hacemos la primera condición dentro del ciclo while
         break
-    elif contraseña_ingresada != contraseña_establecida:
+    elif contraseña_ingresada != contraseña_establecida:  # Hacemos la segunda condición dentro del ciclo while
         intentos -=1
         print(f'Contraseña incorrecta, le quedan {intentos} intentos')
-    elif contraseña_ingresada == contraseña_establecida:
+    elif contraseña_ingresada == contraseña_establecida: # Hacemos la tercera condición dentro del ciclo while
         print('Acceso concedido')
     break
  
-if intentos == 0:
-        print(f'Acceso bloqueado')
+if intentos == 0: # Hacemos una ultima condicional para cuando el numero de intentos es igual a cero fuera del ciclo while
+    print(f'Acceso bloqueado')
 
     
